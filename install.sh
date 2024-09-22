a1119a2f4d189f5896907700c90135a873bb56e4
eudes <eudes8@gmail.com>
2024-09-22 18:32:12 +0200

    remove -gnome from UUID

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mkdir ~/.local/share/gnome-shell/extensions/twingate-status-gnome@eudes.es
-cp -R icons/ extension.js metadata.json stylesheet.css LICENSE ~/.local/share/gnome-shell/extensions/twingate-status-gnome@eudes.es/
+mkdir ~/.local/share/gnome-shell/extensions/twingate-status@eudes.es
+cp -R icons/ extension.js metadata.json stylesheet.css LICENSE ~/.local/share/gnome-shell/extensions/twingate-status@eudes.es/
 echo 'Run the following inside the new shell if not already enabled'
-echo 'gnome-extensions enable twingate-status-gnome@eudes.es'
+echo 'gnome-extensions enable twingate-status@eudes.es'
